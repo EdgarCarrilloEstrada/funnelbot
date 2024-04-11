e3aaa017378b0408fe571fead1395f50c2eda84a
Edgar Carrillo Estrada <edgar.carrillo.e@gmail.com>
2024-04-10 19:59:47 -0600

    Configured docker compose and checks to wait for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -16,8 +16,8 @@ class CalcTests(SimpleTestCase):
 
         self.assertEqual(res, 11)
 
-    def test_substract_numbers(self):
+    def test_subtract_numbers(self):
         """Test substracting numbers."""
-        res = calc.substract(10, 15)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
